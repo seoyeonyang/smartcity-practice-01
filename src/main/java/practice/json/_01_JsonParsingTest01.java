c9a3dd6085b842c39e16533eb9865937d6395711
Seoyeon <acc8837@naver.com>
2022-05-02 10:50:15 +0900

    Json data extract

--- src/main/java/practice/json/_01_JsonParsingTest01.java
+++ src/main/java/practice/json/_01_JsonParsingTest01.java
@@ -2,6 +2,7 @@ package practice.json;
 
 import java.io.IOException;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import practice.util.JsonParsingUtil;
@@ -14,8 +15,28 @@ public class _01_JsonParsingTest01 {
 		
 		JSONObject json = JsonParsingUtil.jsonFileToJsonObject(filePath);
 		
-		System.out.println(json);				
+		JSONObject response = json.getJSONObject("response");
+		JSONObject body = response.getJSONObject("body");
+		JSONObject items = body.getJSONObject("items");
+		JSONArray itemArr = items.getJSONArray("item");
 		
+		for(int i = 0; i < itemArr.length() ; i++) {
+			JSONObject item = itemArr.getJSONObject(i);
+			
+			
+			String buffer = "";
+			buffer += "측정시간 : " + item.getString("mesurede") + ", ";
+			buffer += "맛 : " + item.getString("item1") + ", ";
+			buffer += "냄새 : " + item.getString("item2") + ", ";
+			buffer += "색도 : " + item.getString("item3") + ", ";
+			buffer += "pH : " + item.getString("item4") + ", ";
+			buffer += "탁도 : " + item.getString("item5") + ", ";
+			buffer += "잔류염소수 : " + item.getString("item6");
+			
+			System.out.println(buffer);
+		}
+		
+
 	}
 	
 }
